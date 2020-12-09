b46cfd00b20d1f9b8f900c964444f31b2947a440
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-09 16:20:41 -0500

    Brew formula update for astro version v0.23.2

--- Formula/astro.rb
+++ Formula/astro.rb
@@ -2,16 +2,16 @@
 class Astro < Formula
   desc "To build and run Airflow DAGs locally via docker-compose and deploy DAGs to Astronomer-managed Airflow clusters and interact with the Astronomer API."
   homepage "https://astronomer.io"
-  version "0.23.1"
+  version "0.23.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/astronomer/astro-cli/releases/download/v0.23.1/astro_0.23.1_darwin_amd64.tar.gz"
-    sha256 "d6d0390614d8e597919b9b8ebc6c5a1ba625dc1567c4fe7d355ff9fb774c96b4"
+    url "https://github.com/astronomer/astro-cli/releases/download/v0.23.2/astro_0.23.2_darwin_amd64.tar.gz"
+    sha256 "8f699056df4dd0241fcbbd88b33ba8f592c49102c1fe93a2332db9fe5af0c9b4"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/astronomer/astro-cli/releases/download/v0.23.1/astro_0.23.1_linux_amd64.tar.gz"
-      sha256 "76818a00a279ae39d83f2df65de77b05b3c7153929265a7ecabdfa5653344e8b"
+      url "https://github.com/astronomer/astro-cli/releases/download/v0.23.2/astro_0.23.2_linux_amd64.tar.gz"
+      sha256 "4b538b6661e1bfe8b73107c55cc1708d0eac0f791096842a3e92e4555e705fc9"
     end
   end
 
