7e7e88d45571b39a2f83fa5a050ec9758a5d0a20
neel-astro <neel.dalsania@astronomer.io>
2022-07-18 22:07:19 +0530

    added formula for 1.3.0, 0.29.1 & 0.28.2 CLI releases

--- Formula/astro.rb
+++ Formula/astro.rb
@@ -5,27 +5,27 @@
 class Astro < Formula
   desc "To build and run Airflow DAGs locally via docker-compose and deploy DAGs to Astronomer-managed Airflow clusters and interact with the Astronomer API."
   homepage "https://astronomer.io"
-  version "1.2.0"
+  version "1.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/astronomer/astro-cli/releases/download/v1.2.0/astro_1.2.0_darwin_arm64.tar.gz"
-      sha256 "438ac3f8ebe357f8b5cf9c287468c479873493d2df6b85635ec7b58e7a6e0200"
+      url "https://github.com/astronomer/astro-cli/releases/download/v1.3.0/astro_1.3.0_darwin_arm64.tar.gz"
+      sha256 "a273704fd300d1e932001bf9a016b4f131cd857c2c8fe195ea92c85aceb6ab96"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/astronomer/astro-cli/releases/download/v1.2.0/astro_1.2.0_darwin_amd64.tar.gz"
-      sha256 "223e67194e7b9ce3e384dc629540358292695dd97071210ac93b8ed9f204362d"
+      url "https://github.com/astronomer/astro-cli/releases/download/v1.3.0/astro_1.3.0_darwin_amd64.tar.gz"
+      sha256 "26bfac0dce4cd897620bfd8f35d654e7dbab91dfe5eb95f43289aa0d06852bdc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/astronomer/astro-cli/releases/download/v1.2.0/astro_1.2.0_linux_arm64.tar.gz"
-      sha256 "111ec56a355ff005c8b2cf523d6beeb6af29e7372a507fc10a00e6da428c3e7e"
+      url "https://github.com/astronomer/astro-cli/releases/download/v1.3.0/astro_1.3.0_linux_arm64.tar.gz"
+      sha256 "cc92bcdd8f446a445590b6495532d61e2bc7c690792f5d3d0b500410afd27515"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/astronomer/astro-cli/releases/download/v1.2.0/astro_1.2.0_linux_amd64.tar.gz"
-      sha256 "ff262807f09bf92c03e332ee8bf0832eee636d63a1801e97c8833807aaf61888"  
+      url "https://github.com/astronomer/astro-cli/releases/download/v1.3.0/astro_1.3.0_linux_amd64.tar.gz"
+      sha256 "df4dbe4b08545718f6f720a14c8bcf8e0416518a0dbf3d807529b6fbaae3bd61"  
     end
   end
 
